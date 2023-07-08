cd398d65b8bac6b5334dc4806177dbece4773f73
Corey Clark <coreychristianclark@gmail.com>
2023-07-07 21:23:03 -0400

    Messed around with the formatting of the input fields. I still have some work to do on it. Also need to figure out why my calculations stopped working

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", (e) => {
   const billInput = document.querySelector("#billInput");
   const tipPercentageInput = document.querySelector("#tipPercentageInput");
   const divideByInput = document.querySelector("#divideByInput");
-  const grandTotal = document.querySelector("#grandTotal");
+  const grandTotalText = document.querySelector("#grandTotal");
 
   const billTotal = parseFloat(billInput.value);
   const tipPercentage = parseFloat(tipPercentageInput.value) / 100;
@@ -17,18 +17,20 @@ form.addEventListener("submit", (e) => {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
-    grandTotal.innerText = "$" + formattedTotal;
-    
-    const billTotalText = document.querySelector('#billTotalText')
-    billTotalText.innerText = "$" + billTotal
+  grandTotalText.innerText = "$" + formattedTotal;
 
-
-
-
-
-
-
+  const billTotalText = document.querySelector("#billTotalText");
+  billTotalText.innerText = "$" + billTotal;
 });
+
+function formatNumber(input) {
+    const value = input.value.replace(/[^\d]/g, "").slice(0, 7);
+    const numericValue = value.replace(/,/g, "");
+
+  const formattedValue = Number(numericValue).toLocaleString();
+
+  input.value = formattedValue;
+}
 
 // Must enter a number.console.error
 
