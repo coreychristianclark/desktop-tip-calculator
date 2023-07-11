ceef40234b4968d43ba1bad843664376501abe70
Corey Clark <coreychristianclark@gmail.com>
2023-07-11 17:14:13 -0400

    The decimal places finally work! Now I need to work on the bill input being able to take more than 4 characters

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
+    e.preventDefault();
+    
+    function formatNumber(value, minimumFractionDigits, maximumFractionDigits) {
+      const stringValue = String(value);
+        const numericValue = stringValue.replace(/[^\d.]/g, "").slice(0, 7);
+      const formattedValue = Number(numericValue).toLocaleString(undefined, {
+        minimumFractionDigits: minimumFractionDigits,
+        maximumFractionDigits: maximumFractionDigits,
+      });
+        return formattedValue
+    }
   const billInput = document.querySelector("#billInput");
   const tipPercentageInput = document.querySelector("#tipPercentageInput");
   const divideByInput = document.querySelector("#divideByInput");
@@ -10,46 +19,65 @@ form.addEventListener("submit", (e) => {
 
   const billTotal = parseFloat(billInput.value);
   const tipPercentage = parseFloat(tipPercentageInput.value) / 100;
-  const divideBy = parseFloat(divideByInput.value);
+    const divideBy = parseFloat(divideByInput.value);
 
-  const calculation = billTotal * tipPercentage + billTotal;
+  const calculation = billTotal + (tipPercentage * billTotal);
   const formattedTotal = calculation.toLocaleString(undefined, {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   });
-  grandTotalText.innerText = "$" + formattedTotal;
+    grandTotalText.innerText = "$" + formattedTotal;
+    
+        
 
   const billTotalText = document.querySelector("#billTotalText");
-    billTotalText.innerText = "$" + billTotal;
+    billTotalText.innerText = "$" + billTotal
+    
     
     const tipTotalText = document.querySelector('#tipTotalText')
-    tipTotalText.innerText = "$" + (billTotal * tipPercentage)
+    const tipTotal = billTotal * tipPercentage
+    tipTotalText.innerText = "$" + formatNumber(tipTotal, 2, 2)
 
     const tipEachText = document.querySelector('#tipEachText')
-    tipEachText.innerText = '$' + (billTotal * tipPercentage) / divideBy
+    const tipEach = (billTotal * tipPercentage) / divideBy
+    tipEachText.innerText = '$' + formatNumber(tipEach, 2, 2)
 
     const totalEachText = document.querySelector('#totalEachText')
-    totalEachText.innerText = '$' + (calculation / divideBy)
+    const totalEach = calculation / divideBy
+    totalEachText.innerText = '$' + formatNumber(totalEach, 2, 2)
 
     const fifteen = document.querySelector('#fifteen')
-    fifteen.innerText = '15%: $' + billTotal * 0.15
+    const fifteenMath = billTotal * 0.15
+    fifteen.innerText = '15%: $' + formatNumber(fifteenMath, 2, 2)
 
     const twenty = document.querySelector('#twenty')
-    twenty.innerText = '20%: $' + billTotal * 0.20
+    const twentyMath = billTotal * 0.20
+    twenty.innerText = '20%: $' + formatNumber(twentyMath, 2, 2)
 
     const twentyFive = document.querySelector('#twentyFive')
-    twentyFive.innerText = '25%: $' + billTotal * 0.25
+    const twentyFiveMath = billTotal * 0.25
+    twentyFive.innerText = '25%: $' + formatNumber(twentyFiveMath, 2, 2)
+
+    
 
 });
 
-function formatNumber(input) {
-    const value = input.value.replace(/[^\d]/g, "").slice(0, 7);
-    const numericValue = value.replace(/,/g, "");
+// function formatNumber(value, minimumFractionDigits, maximumFractionDigits) {
+//   const numericValue = value.replace(/[^\d.]/g, "").slice(0, 7);
+//   const formattedValue = Number(numericValue).toLocaleString(undefined, {
+//     minimumFractionDigits: minimumFractionDigits,
+//     maximumFractionDigits: maximumFractionDigits,
+//   });
+//   return formattedValue;
+// }
 
-  const formattedValue = Number(numericValue).toLocaleString();
+// function formatNumber(input) {
+//   const value = input.value.replace(/,/g, "");
+//   const numericValue = value.replace(/[^\d.]/g, "").slice(0, 7);
+//   const formattedValue = Number(numericValue).toLocaleString();
 
-  input.value = formattedValue;
-}
+//   input.value = formattedValue;
+// }
 
 // Must enter a number.console.error
 
